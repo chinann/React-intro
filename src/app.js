03586ad017e79a16a5d268d6635cd88a1ebc663e
chinann <chinan_19nd@hotmail.co.th>
2016-10-15 12:19:10 +0700

    'EditReactComponent'

--- src/app.js
+++ src/app.js
@@ -1,13 +1,85 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = 'My React App'
+// const title = 'My React Application'
 
-const App = () => {
+// const MyApp = () => {
+//     return (
 
-    return <div> My React App </div>
+//     )
+// }
+// or
 
+// const App = () => (
+//     <Header />
+// )
+// or
+
+const SearchForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button type="submit">search</button>
+        </form>
+    )
 }
+
+const Header = (props) => (
+    <header>
+        <h1>{props.title}</h1>
+        <SearchForm />
+    </header>
+)
+
+const Items = (props) => {
+    console.log(props.items)
+    return (
+        <ul>
+        {props.items.map(item => (
+            <li>{item}</li>
+        ))}
+        </ul>
+    )
+}
+
+const Content = (props) => (
+    <section>
+        <p>{props.description}</p>
+        <Items items={props.items}/>
+    </section>
+)
+
+
+const AppWithoutDescription = () => (
+    <Header title="No description here" />
+)
+ const App = () => {
+    const appTitle = 'Fronttechs: React'
+    const description = 'A simple react application'
+    const items= [
+        "Oliver",
+        "Tobey",
+        "Charlie",
+        "Lucky"
+    ]
+    return(
+        <section>
+            <Header title={appTitle} />
+            <Content 
+                description={description} 
+                items={items}
+            />
+        </section>
+    )
+ }
+
+// const App = () => (
+//     <header>
+//         <h1>My React App</h1>
+//     </header>
+// )
+
+
 
 const element = document.getElementById('app')
 ReactDOM.render(<App />, element)
